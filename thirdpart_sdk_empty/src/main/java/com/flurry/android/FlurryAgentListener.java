f6bbc8fbddcd7ca9060fc25ee02eb0aeaf85247f
linan109 <linan@hinterstellar.com>
2018-08-10 11:14:22 +0800

    update version

--- thirdpart_sdk_empty/src/main/java/com/flurry/android/FlurryAgentListener.java
+++
@@ -1,8 +0,0 @@
-package com.flurry.android;
-
-/**
- * Created by newsdog on 21/2/17.
- */
-public interface FlurryAgentListener {
-    void onSessionStarted();
-}
--- thirdpart_sdk_empty/src/main/java/com/xiaomi/mipush/sdk/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.xiaomi.mipush.sdk;
-
-/**
- * Created by newsdog on 21/2/17.
- */
-
-public class ErrorCode {
-    public static final int SUCCESS = 200;
-}
